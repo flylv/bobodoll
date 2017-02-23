aea48b982fe9ebcb9c7e5e0baa0f40660cba280b
Fei LU <fei.lu@meanings.fr>
2017-02-23 11:32:26 +0100

    product page

--- wp-content/themes/bobodoll-gromf/template-product.php
+++ wp-content/themes/bobodoll-gromf/template-product.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: News Page
+ * Template Name: Product Page
  */
 ?>
 
@@ -8,8 +8,7 @@
 
 <section class="product-list slideInUp animated">
 	<div class="row" data-equalizer data-equalize-by-row="true">
-	
- 	<div class="small-12 medium-12 large-12 columns">
+	<div class="small-12 medium-12 large-12 columns">
 	 	<div class="inside-content fadeIn animated">
 	 		<h1 class="entry-title"><?php the_title(); ?></h1>
 		   	<div class="entry-content">
@@ -18,10 +17,9 @@
 	 	</div>
 	 	
     </div>
-
 		<?php
 		$args = array (
-			'post_type'              => array( 'post' ),
+			'post_type'              => array( 'product' ),
 			'posts_per_page'         => -1,
 			'order'                  => 'DESC',
 			'orderby'                => 'date'		
@@ -30,6 +28,7 @@
 		$the_query = new WP_Query( $args );?>
 
 		<?php if ( $the_query->have_posts() ) : ?>
+
 
 				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
